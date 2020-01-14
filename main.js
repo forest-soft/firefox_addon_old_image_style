87c444b7fc46033ae8d8c3d0cbe77fdc7065fca2
forest-soft <kimoto@forest-soft.net>
2020-01-14 16:22:41 +0900

    v1.1のソース

---
+++ main.js
@@ -0,0 +1,17 @@
+try {
+	if (document && document.body) {
+		if (document.body.childNodes.length == 1 && document.body.childNodes[0].tagName == "IMG") {
+			var image_element = document.body.childNodes[0];
+			
+			// 背景の黒画像を消す。
+			document.body.style.background = "none";
+			image_element.style.background = "none";
+			
+			// センター表示を打ち消す。
+			image_element.style.right = "unset";
+			image_element.style.bottom = "unset";
+		}
+	}
+} catch(error) {
+	null;
+}
