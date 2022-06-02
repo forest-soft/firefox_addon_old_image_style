6a250c2e25eb47cb8725ff7d03b50fee0f4b99a9
森林ソフト <kimoto@forest-soft.net>
2022-06-02 10:24:00 +0900

    新しいバージョンのFirefoxで背景色が変わらなくなっていたので対策した。

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ try {
 			var image_element = document.body.childNodes[0];
 			
 			// 背景の黒画像を消す。
+			document.documentElement.style.background = "none";
 			document.body.style.background = "none";
 			image_element.style.background = "none";
 			
